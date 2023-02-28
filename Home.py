4a6e67f54b26c623276cb033276cd4c9578d5902
ayeshafarheen987 <113899070+ayeshafarheen987@users.noreply.github.com>
2023-02-28 12:05:05 +0530

    Delete Home.py

--- Home.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-st.title(':red[Hello] Everyone! :woman-raising-hand:')
-st.header("""My name is:blue[ Ayesha Farheen], and I developed a dashboard for predicting FIFA players' market value using streamlit.""")
-
-
-st.subheader('you can access my project.')
-st.write("#### Github   [link](https://github.com/ayeshafarheen987/FIFA_Player_Valuation)")
-st.subheader('lets connect.')
-st.write('#### Linkedin  [link](https://www.linkedin.com/in/ayesha-farheen-950272221/)')
