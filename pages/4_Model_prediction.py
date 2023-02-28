6024d27de67bfa32d2e776c7653a4933d080d9e3
ayeshafarheen987 <113899070+ayeshafarheen987@users.noreply.github.com>
2023-02-28 12:56:11 +0530

    Update 4_Model_prediction.py

--- pages/4_Model_prediction.py
+++ pages/4_Model_prediction.py
@@ -34,7 +34,7 @@ Potential = st.number_input("Enter Potential",0,100)
 
 if st.button("Predict"):
 
-    pickle_in = open('streamlit_web_app/blob/main/model_Random.pkl', 'rb') 
+    pickle_in = open('https://github.com/ayeshafarheen987/streamlit_web_app/blob/main/model_Random.pkl', 'rb') 
     r = pickle.load(pickle_in)
     
     X = pd.DataFrame([[Best_overall,Overall_rating,Potential,Age]], columns = ['Best_overall','Overall_rating','Potential','Age'])
